6ba7eb4528fe35c561c401cddb1ec4c894c0177c
Ed Wildgoose <git@wildgooses.com>
2015-06-09 20:29:26 +0100

    add :exclude_nil parameter to decoder options

--- lib/xml_rpc/decoder.ex
+++ lib/xml_rpc/decoder.ex
@@ -28,7 +28,7 @@ defmodule XMLRPC.Decoder do
   * XMLRPC.MethodResponse
   * XMLRPC.Fault
   """
-  def decode!(iodata, _options) do
+  def decode!(iodata, options) do
     {:ok, model} = :erlsom.compile_xsd(@xmlrpc_xsd)
     xml = IO.iodata_to_binary(iodata)
 
@@ -40,7 +40,7 @@ defmodule XMLRPC.Decoder do
       {:error, message} when is_list(message) ->
           raise DecodeError, message: List.to_string(message)
       {:ok, struct, _rest} ->
-          parse(struct)
+          parse(struct, options)
     end
 
   end
@@ -51,20 +51,22 @@ defmodule XMLRPC.Decoder do
 
   # Parse a method 'Call'
   defp parse(  {:methodCall, [], method_name,
-                {:"methodCall/params", [], params }} )
+                {:"methodCall/params", [], params }},
+               options )
       when is_list(params)
   do
-    %MethodCall{ method_name: method_name, params: parse_params(params) }
+    %MethodCall{ method_name: method_name, params: parse_params(params, options) }
   end
 
   # Parse a 'fault' Response
   defp parse(  {:methodResponse, [],
                 {:"methodResponse/fault", [],
                   {:"methodResponse/fault/value", [],
-                    {:"methodResponse/fault/value/struct", [], fault_struct} }}} )
+                    {:"methodResponse/fault/value/struct", [], fault_struct} }}},
+                options )
       when is_list(fault_struct)
   do
-    fault = parse_struct(fault_struct)
+    fault = parse_struct(fault_struct, options)
     fault_code = Dict.get(fault, "faultCode")
     fault_string = Dict.get(fault, "faultString")
     %Fault{ fault_code: fault_code, fault_string: fault_string }
@@ -72,43 +74,44 @@ defmodule XMLRPC.Decoder do
 
   # Parse any other 'Response'
   defp parse(  {:methodResponse, [],
-                {:"methodResponse/params", [], param}} )
+                {:"methodResponse/params", [], param}},
+               options )
       when is_tuple(param)
   do
-    %MethodResponse{ param: parse_param(param) }
+    %MethodResponse{ param: parse_param(param, options) }
   end
 
   # ##########################################################################
 
   # Parse an 'array' atom
-  defp parse_value( {:ValueType, [], [{:ArrayType, [], {:"ArrayType/data", [], array}}]} )
+  defp parse_value( {:ValueType, [], [{:ArrayType, [], {:"ArrayType/data", [], array}}]}, options )
       when is_list(array)
   do
-    parse_array(array)
+    parse_array(array, options)
   end
 
   # Parse a 'struct' atom
-  defp parse_value( {:ValueType, [], [{:StructType, [],                   struct}]} )
+  defp parse_value( {:ValueType, [], [{:StructType, [],                   struct}]}, options)
       when is_list(struct)
   do
-    parse_struct(struct)
+    parse_struct(struct, options)
   end
 
   # Parse an 'integer' atom
-  defp parse_value( {:ValueType, [], [{:"ValueType-int", [],              int}]} )
+  defp parse_value( {:ValueType, [], [{:"ValueType-int", [],              int}]}, _options)
       when is_integer(int)
   do
       int
   end
 
   # Parse a 'float' atom
-  defp parse_value( {:ValueType, [], [{:"ValueType-double", [],           float}]} ) do
+  defp parse_value( {:ValueType, [], [{:"ValueType-double", [],           float}]}, _options) do
     Float.parse(float)
     |> elem(0)
   end
 
   # Parse a 'boolean' atom
-  defp parse_value( {:ValueType, [], [{:"ValueType-boolean", [],          boolean}]} ) do
+  defp parse_value( {:ValueType, [], [{:"ValueType-boolean", [],          boolean}]}, _options) do
     case boolean do
       "0" -> false
       "1" -> true
@@ -116,19 +119,23 @@ defmodule XMLRPC.Decoder do
   end
 
   # Parse a 'datetime' atom (needs decoding from bolloxed iso8601 alike format...)
-  defp parse_value( {:ValueType, [], [{:"ValueType-dateTime.iso8601", [], datetime}]} ) do
+  defp parse_value( {:ValueType, [], [{:"ValueType-dateTime.iso8601", [], datetime}]}, _options) do
     %XMLRPC.DateTime{raw: datetime}
   end
 
   # Parse a 'string' atom
-  defp parse_value( {:ValueType, [], [{:"ValueType-string", [],           string}]} ) do
+  defp parse_value( {:ValueType, [], [{:"ValueType-string", [],           string}]}, _options) do
     string
   end
 
   # Parse a 'nil' atom
   # Note: this is an xml-rpc extension
-  defp parse_value( {:ValueType, [], [NilType: []]} ) do
-    nil
+  defp parse_value( {:ValueType, [], [NilType: []]}, options) do
+    if options[:exclude_nil] do
+      raise XMLRPC.DecodeError, message: "unable to decode <nil/>"
+    else
+      nil
+    end
   end
 
   # ##########################################################################
@@ -136,11 +143,11 @@ defmodule XMLRPC.Decoder do
   # Parse the 'struct'
   # 'structs' are a list of key-value pairs
   # Note: values can be 'structs'/'arrays' as well as other atom types
-  defp parse_struct(doc) when is_list(doc) do
+  defp parse_struct(doc, options) when is_list(doc) do
     doc
     |> Enum.reduce  Map.new,
                     fn(member, acc) ->
-                        parse_member(member)
+                        parse_member(member, options)
                         |> Enum.into acc
                     end
   end
@@ -148,28 +155,27 @@ defmodule XMLRPC.Decoder do
   # Parse the 'array'
   # 'arrays' are just an ordered list of other atom values
   # Note: values can be 'structs'/'arrays' as well as other atom types
-  defp parse_array(doc) when is_list(doc) do
+  defp parse_array(doc, options) when is_list(doc) do
     doc
-    |> Enum.map &parse_value/1
+    |> Enum.map fn v -> parse_value(v, options) end
   end
 
   # ##########################################################################
 
   # Parse a list of Parameter values (implies a Request)
-  defp parse_params( values ) when is_list(values) do
+  defp parse_params(values, options) when is_list(values) do
     values
-    |> Enum.map &parse_param/1
+    |> Enum.map fn p -> parse_param(p, options) end
   end
 
   # Parse a single Parameter
-  defp parse_param( {:ParamType, [], value } ), do: parse_value(value)
+  defp parse_param( {:ParamType, [], value }, options ), do: parse_value(value, options)
 
   # ##########################################################################
 
   # Parse one member of a Struct
-  defp parse_member( {:MemberType, [], name, value } ) do
-    [{name, parse_value(value)}]
+  defp parse_member( {:MemberType, [], name, value }, options ) do
+    [{name, parse_value(value, options)}]
   end
-
 
 end
