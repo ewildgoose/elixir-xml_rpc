0651a7ea4623948268f0f8c5f6f90f50e51d16f6
Ed Wildgoose <git@wildgooses.com>
2017-02-13 17:07:44 +0000

    bump dependencies

--- mix.exs
+++ mix.exs
@@ -31,8 +31,8 @@ defmodule XmlRpc.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [   {:earmark, "~> 0.1", only: :docs},
-        {:ex_doc, "~> 0.7", only: :docs},
+    [   {:earmark, "~> 1.0", only: :docs},
+        {:ex_doc, "~> 0.14", only: :docs},
         {:erlsom, "~> 1.4"},
     ]
   end
