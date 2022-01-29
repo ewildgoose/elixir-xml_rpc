efc400bb2c5072444b24a5d9c9ed62ab4c258736
Kian-Meng Ang <kianmeng.ang@gmail.com>
2022-01-30 04:04:54 +0800

    Misc doc changes (#19)
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- lib/xml_rpc/base64.ex
+++ lib/xml_rpc/base64.ex
@@ -1,23 +1,23 @@
 defmodule XMLRPC.Base64 do
   @moduledoc """
-  Elixir datatype to store base64 value
+  Elixir datatype to store base64 value.
 
-  Note: See the `Base` module for other conversions in Elixir stdlib
+  Note: See the `Base` module for other conversions in Elixir stdlib.
   """
   @type t :: %__MODULE__{raw: String.t}
   defstruct raw: ""
 
   @doc """
-  Create a new Base64 struct from an binary input
+  Create a new Base64 struct from an binary input.
   """
   def new(binary) do
     %__MODULE__{raw: Base.encode64(binary)}
   end
 
   @doc """
-  Attempt to decode a Base64 encoded value
+  Attempt to decode a Base64 encoded value.
 
-  Note: thin wrapper around `Base.decode64/1`
+  Note: thin wrapper around `Base.decode64/1`.
   """
   def to_binary(%__MODULE__{raw: encoded}) do
     # Some XMLRPC libraries put whitespace in the Base64 data.
